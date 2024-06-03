06460bf97608752b95389fc2c7f5f3dd09c09fec
Viktor Bachmanov <vbachmanov@mail.ru>
2024-06-03 08:08:36 +0300

    LoginDialog has been done

--- src/runtime/components/LoginDialog.vue
+++ src/runtime/components/LoginDialog.vue
@@ -13,7 +13,7 @@ const checked = ref(false)
 
 const loading = defineModel()
 
-const emit = defineEmits(['goToRegister'])
+const emit = defineEmits(['close', 'goToRegister', 'goToForgetEmail'])
 
 function handleRegister() {
   emit('goToRegister')
@@ -33,6 +33,7 @@ function togglePasswordInputType() {
     : 'password'
 }
 
+
 </script>
 
 
@@ -51,7 +52,7 @@ function togglePasswordInputType() {
       Телеграмм бот
     </NuxtLink>
 
-    <div class="border-t border-[#EAEAEA] py-4 text-sm text-[#707070]">
+    <div class="border-t border-[#EAEAEA] py-4 text-sm text-[#707070] indent-1">
       Введите данные для авторизации
     </div>
 
@@ -79,6 +80,7 @@ function togglePasswordInputType() {
       colorLight="#6F46C4"
       bgColorDark="#1e293b"
       :error="error"
+      :errorSpace="false"
     >
       <template #prefixIcon>
         <IconUnlock class="w-[20px]" />
@@ -89,15 +91,33 @@ function togglePasswordInputType() {
       </template>
     </InputVB>
 
+    <div 
+        class="text-sm text-[#707070] cursor-pointer text-right mt-1 mr-2"
+        @click="emit('goToForgetEmail')"
+      >
+      Забыли пароль?
+    </div>
+
     <CheckboxVB 
       v-model="checked" 
-      postfixLabel="Test checkbox"
+      postfixLabel="Оставаться авторизованным после входа"
       id="rememberCheckbox"
+      labelClass="text-sm font-normal"
+      class="mt-3 ml-3"
+      borderClass="dark:text-gray-500"
     />
+
+    <button 
+      type="button"
+      @click="handleLogin"
+      class="mt-7 flex items-center rounded-[20px] w-full justify-center border-none p-5 bg-black dark:bg-zinc-800 shadow-none font-bold max-w-full text-lg text-white"
+    >
+        Продолжить
+    </button>
 
     <div
       @click="handleRegister"
-      class="cursor-pointer"
+      class="text-center text-sm font-semibold cursor-pointer mt-6"
     >
       Нет аккаунта?
     </div>
