a5a452a426e8503f7aa080d8e2ab619c79e99d85
Viktor Bachmanov <vbachmanov@mail.ru>
2024-06-03 10:08:04 +0300

    Import 'backendFetch'

--- src/runtime/components/LoginDialog.vue
+++ src/runtime/components/LoginDialog.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useRuntimeConfig } from '#app'
-import { useUser } from '#imports'
+import { useUser, backendFetch, backendFetchXSRF } from '#imports'
 
 const { $toast } = useNuxtApp();
 
